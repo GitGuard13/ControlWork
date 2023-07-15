425f76c5a43c2f041daec91465d82580b680741b
Aleks <125861643+GitGuard13@users.noreply.github.com>
2023-07-15 11:57:01 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите массив строк через запятую:");
+        string[] inputArr = Console.ReadLine().Split(",");
+
+        int count = 0;
+        for (int i = 0; i < inputArr.Length; i++)
+        {
+            if (inputArr[i].Length <= 3)
+                count++;
+        }
+
+        string[] resultArr = new string[count];
+        int index = 0;
+        for (int i = 0; i < inputArr.Length; i++)
+        {
+            if (inputArr[i].Length <= 3)
+            {
+                resultArr[index] = inputArr[i];
+                index++;
+            }
+        }
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine("[" + string.Join(", ", resultArr) + "]");
+    }
+}
